be24e797a2d5b4ead8836f23b9d3efc796f6ad89
Ovchinnikov19 <r1dd1k1919@gmail.com>
2018-05-13 20:04:55 +0500

    add fetchListItems and as? in fetchObjectCard

--- Computer database/Computer database/APIManager.swift
+++ Computer database/Computer database/APIManager.swift
@@ -24,10 +24,10 @@ func fetchObjectCard(idObjectCard: Int){
         do {
             let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
             //print(json)
-            if let id = json["id"]{
+            if let id = json["id"] as? Int{
                 print("id = \(id)")
             }
-            if let name = json["name"] {
+            if let name = json["name"] as? String{
                 print("name = \(name)")
             }
             if let introduced = json["introduced"]{
@@ -59,3 +59,40 @@ func fetchObjectCard(idObjectCard: Int){
     task.resume()
 }
 
+func fetchListItems(page: Int){
+    var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers?p=\(page)")!)
+    request.httpMethod = "GET"
+    
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    let session = URLSession.shared
+    let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+        //print(response!)
+        do {
+            let jsonArray = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+            //print(json)
+            if let items: [AnyObject] = jsonArray["items"] as? [AnyObject]{
+                for json in items {
+                    if let idItem = json["id"] as? Int{
+                        print("id = \(idItem)")
+                    }
+                    if let nameItem = json["name"] as? String{
+                        print("name = \(nameItem)")
+                    }
+                    
+                    if let company: [String: AnyObject] = json["company"] as? [String : AnyObject]{
+                        if let idCompany = company["id"] as? Int{
+                            print("idCompany = \(idCompany)")
+                        }
+                        if let nameCompany = company["name"] as? String{
+                            print("nameCompany = \(nameCompany)")
+                        }
+                    }
+                    print("")
+                }
+            }
+        } catch {
+            print("error")
+        }
+})
+task.resume()
+}
