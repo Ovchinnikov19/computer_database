988d0883e05a69177ac93103044154192b270082
Ovchinnikov19 <r1dd1k1919@gmail.com>
2018-05-14 16:21:55 +0500

    add table and create class APIManagerListItems

--- Computer database/Computer database/APIManager.swift
+++ Computer database/Computer database/APIManager.swift
@@ -8,6 +8,55 @@
 
 import Foundation
 
+struct ItemDatabase{
+    var idItem: Int?
+    var nameItem: String?
+    var idCompany: Int?
+    var nameCompany: String?
+}
+
+class APIManagerListItems {
+    
+    var arrayItems: [ItemDatabase] = []
+    
+    func fetchListItems(page: Int){
+        var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers?p=\(page)")!)
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                let jsonArray = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                if let items: [AnyObject] = jsonArray["items"] as? [AnyObject]{
+                    for json in items {
+                        var itemDatabase = ItemDatabase()
+                        if let idItem = json["id"] as? Int{
+                            itemDatabase.idItem = idItem
+                        }
+                        if let nameItem = json["name"] as? String{
+                            itemDatabase.nameItem = nameItem
+                        }
+                        
+                        if let company: [String: AnyObject] = json["company"] as? [String : AnyObject]{
+                            if let idCompany = company["id"] as? Int{
+                                itemDatabase.idCompany = idCompany
+                            }
+                            if let nameCompany = company["name"] as? String{
+                                itemDatabase.nameCompany = nameCompany
+                            }
+                        }
+                        self.arrayItems.append(itemDatabase)
+                    }
+                }
+            } catch {
+                print("error")
+            }
+        })
+        task.resume()
+    }
+}
+
 func fetchObjectCard(idObjectCard: Int){
     var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers/\(idObjectCard)")!)
     request.httpMethod = "GET"
@@ -54,43 +103,7 @@ func fetchObjectCard(idObjectCard: Int){
     task.resume()
 }
 
-func fetchListItems(page: Int){
-    var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers?p=\(page)")!)
-    request.httpMethod = "GET"
-    
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    let session = URLSession.shared
-    let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
-        //print(response!)
-        do {
-            let jsonArray = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
-            //print(json)
-            if let items: [AnyObject] = jsonArray["items"] as? [AnyObject]{
-                for json in items {
-                    if let idItem = json["id"] as? Int{
-                        print("id = \(idItem)")
-                    }
-                    if let nameItem = json["name"] as? String{
-                        print("name = \(nameItem)")
-                    }
-                    
-                    if let company: [String: AnyObject] = json["company"] as? [String : AnyObject]{
-                        if let idCompany = company["id"] as? Int{
-                            print("idCompany = \(idCompany)")
-                        }
-                        if let nameCompany = company["name"] as? String{
-                            print("nameCompany = \(nameCompany)")
-                        }
-                    }
-                    print("")
-                }
-            }
-        } catch {
-            print("error")
-        }
-})
-task.resume()
-}
+
 
 func fetchSimilarItems(id: Int){
     var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers/\(id)/similar")!)
