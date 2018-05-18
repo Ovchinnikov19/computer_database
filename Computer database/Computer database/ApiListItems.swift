d49be3b7acee9491858caabfc429f5cc7bf9993f
Ovchinnikov19 <r1dd1k1919@gmail.com>
2018-05-18 16:05:25 +0500

    Fill ApuListItems

--- Computer database/Computer database/ApiListItems.swift
+++ Computer database/Computer database/ApiListItems.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+struct ItemDatabase{
+    var idItem: Int?
+    var nameItem: String?
+    var idCompany: Int?
+    var nameCompany: String?
+}
+
+class ApiListItems{
+    var arrayItems: [ItemDatabase] = []
+    
+    func fetchListItems(page: Int){
+        var request = URLRequest(url: URL(string: "http://testwork.nsd.naumen.ru/rest/computers?p=\(page)")!)
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            do {
+                let jsonArray = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                if let items: [AnyObject] = jsonArray["items"] as? [AnyObject]{
+                    for json in items {
+                        var itemDatabase = ItemDatabase()
+                        if let idItem = json["id"] as? Int{
+                            itemDatabase.idItem = idItem
+                            
+                        }
+                        if let nameItem = json["name"] as? String{
+                            itemDatabase.nameItem = nameItem
+                        }
+                        
+                        if let company: [String: AnyObject] = json["company"] as? [String : AnyObject]{
+                            if let idCompany = company["id"] as? Int{
+                                itemDatabase.idCompany = idCompany
+                            }
+                            if let nameCompany = company["name"] as? String{
+                                itemDatabase.nameCompany = nameCompany
+                            }
+                        }
+                        self.arrayItems.append(itemDatabase)
+                    }
+                }
+            } catch {
+                print("error")
+            }
+        })
+        
+        task.resume()
+    }
+}
